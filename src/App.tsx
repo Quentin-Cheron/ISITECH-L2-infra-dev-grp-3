bf1d1c36e69f0b7e1a0229087ccd26a5cb8f9d06
clmaxime <maxime.cluzel@ecole-isitech.fr>
2024-08-29 17:22:22 +0200

    feature: add favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </BrowserRouter>
   );
